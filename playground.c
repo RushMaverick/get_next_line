9d2d78c291dff624fec9006493ff0678017f24f6
Rasmus Rask <rasse.rask@gmail.com>
2023-02-14 14:57:32 +0200

    insert_it added

--- playground.c
+++ playground.c
@@ -3,35 +3,22 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <string.h>
-#define BUFFER 100
+#define BUFFER 10
 
-char *split_it(char *temp, char *buf);
+char *insert_it(int fd, char *temp, char *buf);
 
 char *get_it(int fd)
 {
 	static char *buf;
 	char *temp;
 	temp = malloc(sizeof(char) * BUFFER + 1);
-	read(fd, temp, BUFFER);
-	temp[BUFFER] = '\0';
-	split_it(temp, buf);
-	return (buf);
+	insert_it(fd, temp, buf);
+	return (temp);
 }
 
-char *split_it(char *temp, char *buf)
+char *insert_it(int fd, char *temp, char *buf)
 {
-	int i; 
-
-	i = 0;
-	while (temp[i] != '\0')
-	{
-		if (temp[i] == '\n')
-		{
-			buf = "I have no words.";
-			return (buf);
-		}
-		i++;
-	}
+	read(fd, temp, BUFFER);
 	return(buf);
 }
 
@@ -39,6 +26,10 @@ int main (void)
 {
 	char *huh;
 	int fd = open("moi.txt", O_APPEND);
+	huh = get_it(fd);
+	printf("%s", huh);
+	huh = get_it(fd);
+	printf("%s", huh);
 	huh = get_it(fd);
 	printf("%s", huh);
 	return(0);
