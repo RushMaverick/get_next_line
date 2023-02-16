ae16445d1a54f9d31c7210a4c7943cc812795375
Rasmus Rask <rasse.rask@gmail.com>
2023-02-16 11:42:57 +0200

    strdup and strjoin added to handle an empty stash at start

--- playground.c
+++ playground.c
@@ -24,6 +24,41 @@ char *insert_it(int fd, char *temp, char *stash);
 // 	return (ptr);
 // }
 
+void	*ft_memset(void *b, int c, size_t len)
+{
+	size_t			i;
+	unsigned char	*bcpy;
+
+	bcpy = (unsigned char *)b;
+	i = 0;
+	while (i < len)
+	{
+		bcpy[i] = (unsigned char)c;
+		i++;
+	}
+	return (b);
+}
+
+
+char	*ft_strdup(const char *s1)
+{
+	char	*dup;
+	int		i;
+
+	dup = malloc((sizeof(char) * strlen(s1) + 1)); //change to ft_strlen
+	if (dup == NULL)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		dup[i] = s1[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
+
+
 static void	istheres(char *joined, char const *s1, char const *s2)
 {
 	int	i;
@@ -47,19 +82,21 @@ static void	istheres(char *joined, char const *s1, char const *s2)
 	joined[j] = '\0';
 }
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char const *stash, char const *temp)
 {
 	char	*joined;
 
-	if (!s2)
-		return ((char *)s1);
-	if (!s1)
-		return((char *)s2);
+	if (!temp)
+		return ((char *)stash);
+	if (!stash)
+		return(ft_strdup(temp));
 	else
-		joined = calloc(strlen(s1) + strlen(s2) + 1, sizeof(char));
+	{
+		joined = calloc(strlen(stash) + strlen(temp) + 1, sizeof(char));//change to ft_strlen
+	}
 	if (!joined)
 		return (NULL);
-	istheres(joined, s1, s2);
+	istheres(joined, stash, temp);
 	return (joined);
 }
 
@@ -67,9 +104,7 @@ char	*get_it(int fd)
 {
 	static char	*stash;
 	char		*temp;
-	int 		readbytes;
 
-	readbytes = 1;
 	temp = calloc(BUFFER + 1, sizeof(char)); // remove this
 	stash = insert_it(fd, temp, stash);
 	return (stash);
@@ -77,13 +112,12 @@ char	*get_it(int fd)
 
 char *insert_it(int fd, char *temp, char *stash)
 {
-	// int i;
 	int readbytes;
 
-	// i = 0;
 	readbytes = 1;
 	while (readbytes)
 	{
+		ft_memset(temp, 0, strlen(temp)); //change to ft_strlen
 		readbytes = read(fd, temp, BUFFER);
 		stash = ft_strjoin(stash, temp);
 	}
@@ -96,8 +130,8 @@ int	main(void)
 	char	*huh;
 	int		fd;
 
-	fd = open("moi.txt", O_APPEND);
+	fd = open("moi.txt", O_RDONLY);
 	huh = get_it(fd);
-	printf("%s\n", huh);
+	printf("!%s!\n", huh);
 	return (0);
 }
