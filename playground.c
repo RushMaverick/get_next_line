462eb240c71ad4b0f35136b2228c416eb4c1c67f
Rasmus Rask <rasse.rask@gmail.com>
2023-02-17 15:13:36 +0200

    change the stash, temp and newline relationship to work according to get_next_line standards

--- playground.c
+++ playground.c
@@ -6,23 +6,7 @@
 #define BUFFER 20
 
 char *insert_it(int fd, char *temp, char *stash);
-
-// void	*ft_calloc(size_t count, size_t size)
-// {
-// 	void	*ptr;
-// 	size_t	mult;
-
-// 	if (count == 0 || size == 0)
-// 		return (ft_calloc(1, 1));
-// 	mult = count * size;
-// 	if (mult / count != size)
-// 		return (NULL);
-// 	ptr = malloc(mult);
-// 	if (!ptr)
-// 		return (0);
-// 	ft_bzero(ptr, mult);
-// 	return (ptr);
-// }
+char *handle_newline(char *temp, char *stash);
 
 void	*ft_memset(void *b, int c, size_t len)
 {
@@ -38,7 +22,6 @@ void	*ft_memset(void *b, int c, size_t len)
 	}
 	return (b);
 }
-
 
 char	*ft_strdup(const char *s1)
 {
@@ -57,7 +40,6 @@ char	*ft_strdup(const char *s1)
 	dup[i] = '\0';
 	return (dup);
 }
-
 
 static void	istheres(char *joined, char const *s1, char const *s2)
 {
@@ -92,7 +74,7 @@ char	*ft_strjoin(char const *stash, char const *temp)
 		return(ft_strdup(temp));
 	else
 	{
-		joined = calloc(strlen(stash) + strlen(temp) + 1, sizeof(char));//change to ft_strlen
+		joined = calloc(strlen(stash) + strlen(temp) + 1, sizeof(char));//change to ft_strlen and ft_calloc
 	}
 	if (!joined)
 		return (NULL);
@@ -102,36 +84,73 @@ char	*ft_strjoin(char const *stash, char const *temp)
 
 char	*get_it(int fd)
 {
-	static char	*stash;
+	static char	*stash; // consider using static char for storing the read stuff and strjoin the temp to stash
+	char		*newline;
 	char		*temp;
 
 	temp = calloc(BUFFER + 1, sizeof(char)); // remove this
-	stash = insert_it(fd, temp, stash);
-	return (stash);
+	stash = calloc(BUFFER + 1, sizeof(char)); // remove this
+	newline = insert_it(fd, temp, stash);
+	return (newline);
+}
+
+char *handle_newline(char *temp, char *stash)
+{
+	int i;
+	char *newline;
+
+	i = 0;
+	newline = calloc(strlen(temp) + 1, sizeof(char));//change to ft_strlen and ft_calloc
+
+//	Winner Winner\nChic\nken Di\nnner
+
+	while (temp[i])
+	{
+		if (temp[i] == '\n')
+		{
+			i++;
+			while (temp[i] != '\n' || temp[i] != '\0')
+			{
+				stash[i] = temp[i]; //not allocated memory
+			}
+		}
+		newline[i] = temp[i];
+		i++;
+	}
+	return (newline);
 }
 
 char *insert_it(int fd, char *temp, char *stash)
 {
 	int readbytes;
+	char *newline;
 
 	readbytes = 1;
 	while (readbytes)
 	{
 		ft_memset(temp, 0, strlen(temp)); //change to ft_strlen
 		readbytes = read(fd, temp, BUFFER);
+		newline = handle_newline(temp, stash); //This might have to be handled after the loop. 
 		stash = ft_strjoin(stash, temp);
 	}
+	free(temp);
 	//Check libft for function that checks for a certain character,like null terminator or newline
-	return (stash);
+	return (newline);
 }
 
 int	main(void)
 {
 	char	*huh;
 	int		fd;
+	int		i;
 
+	i = 1;
 	fd = open("moi.txt", O_RDONLY);
-	huh = get_it(fd);
-	printf("!%s!\n", huh);
+	while (i)
+	{
+		huh = get_it(fd);
+		i--;
+		printf("%s\n", huh);
+	}
 	return (0);
 }
