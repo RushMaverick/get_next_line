68c0325f0f650a2fa7836b0be7d5f62d0ca75d7b
Rasmus Rask <rasse.rask@gmail.com>
2023-02-21 13:19:30 +0200

    Next work is finding where to append stash to the start of oneline

--- playground2.c
+++ playground2.c
@@ -8,6 +8,65 @@
 char *handle_newline(char *oneline, char *stash);
 char *get_oneline(char *oneline, char *stash, int fd);
 
+char	*ft_strdup(const char *s1)
+{
+	char	*dup;
+	int		i;
+
+	dup = malloc((sizeof(char) * strlen(s1) + 1)); //change to ft_strlen
+	if (dup == NULL)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		dup[i] = s1[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
+
+static void	istheres(char *joined, char const *s1, char const *s2)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (s1[i] != '\0')
+	{
+		joined[j] = s1[i];
+		i++;
+		j++;
+	}
+	i = 0;
+	while (s2[i] != '\0')
+	{
+		joined[j] = s2[i];
+		i++;
+		j++;
+	}
+	joined[j] = '\0';
+}
+
+char	*ft_strjoin(char const *stash, char const *temp)
+{
+	char	*joined;
+
+	if (!temp)
+		return ((char *)stash);
+	if (!stash)
+		return(ft_strdup(temp));
+	else
+	{
+		joined = calloc(strlen(stash) + strlen(temp) + 1, sizeof(char));//change to ft_strlen and ft_calloc
+	}
+	if (!joined)
+		return (NULL);
+	istheres(joined, stash, temp);
+	return (joined);
+}
+
 char *get_it(fd)
 {
 	static char *stash;
@@ -25,6 +84,7 @@ char *get_oneline(char *oneline, char *stash, int fd)
 
 	readbytes = read(fd, oneline, BUFFER_SIZE);
 	oneline = handle_newline(oneline, stash);
+	oneline = ft_strjoin(oneline, stash);
 	return (oneline);
 }
 //Winner Winner\nChicken Dinner
@@ -32,20 +92,22 @@ char *get_oneline(char *oneline, char *stash, int fd)
 char *handle_newline(char *oneline, char *stash)
 {
 	int i;
+	int stash_index;
 	char *handled_line;
 
 	i = 0;
-	handled_line = calloc(strlen(oneline) + 1, sizeof(char));
+	handled_line = calloc(strlen(oneline) + 1, sizeof(char)); //Count to \n and then calloc correct size
 	while (oneline[i])
 	{
+		stash_index = 0;
 		if (oneline[i] == '\n' || oneline[i] == '\0')
 		{
 			i++;
 			while (oneline[i])
 			{
-				stash[i] = oneline[i];
+				stash[stash_index] = oneline[i];
 				i++;
-				// store_remainder(stash, oneline, i);//Static variable can somehow be used to store where you are at all times.
+				stash_index++;
 			}
 			return(handled_line);
 		}
